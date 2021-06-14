5642a382ab884bb38c8e14ca73568b8cc1b9271c
Akimana J d Amour <akimanaja17@gmail.com>
2021-06-14 13:18:04 +0200

    Create a model and a helper for the logging

---
+++ src/database/model/history.model.js
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose';
+
+const HistorySchema = new Schema(
+  {
+    description: { type: String, required: true },
+    project: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Project',
+    },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    manager: { type: Schema.Types.ObjectId, ref: 'User' },
+    reads: { type: [Schema.Types.ObjectId] },
+  },
+  {
+    timestamps: true,
+    writeConcern: { w: 'majority', j: true, wtimeout: 1000 },
+  },
+);
+
+const History = model('History', HistorySchema);
+
+export default History;
---
+++ src/utils/log.project.js
@@ -0,0 +1,44 @@
+import History from '../database/model/history.model';
+
+const logActions = [
+  'project_create',
+  'project_edit',
+  'project_status',
+  'project_manager',
+  'quote_create',
+  'quote_update',
+  'quote_status',
+  'invoice_approve',
+  'subscription_create',
+];
+
+export const logProject = async (
+  entities = {},
+  action = 'project_create',
+  details = null,
+) => {
+  try {
+    const { project, user, manager } = entities;
+    const descriptions = {
+      project_create: `Project created`,
+      project_edit: `Project edited`,
+      project_status: `Project status changed`,
+      project_manager: `A new manager(${manager.fullName}) assigned`,
+      quote_create: `New quote created by ${user.fullName}`,
+      quote_update: details || `Quote edited`,
+      quote_status: details || `Quote status changed`,
+      invoice_approve: `Invoice approved`,
+      subscription_create: `New subscription created`,
+    };
+    const logAction =
+      logActions.indexOf(action) < 0 ? 'project_create' : action;
+    await History.create({
+      description: descriptions[logAction],
+      project: project.id,
+      user: user?.id,
+      manager: manager?.id,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
