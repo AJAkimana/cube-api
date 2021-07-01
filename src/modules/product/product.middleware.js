199171570a976e117a903450648aa50ae60f59d9
Akimana J d Amour <akimanaja17@gmail.com>
2021-07-01 18:38:30 +0200

    Validate project - Check if a project does exist - Validate the body of a project

---
+++ src/modules/product/product.middleware.js
@@ -0,0 +1,22 @@
+import { schemaErrors } from '../../utils/helpers';
+import { serverResponse } from '../../utils/response';
+import { productSchema } from '../../utils/schema/product.schema';
+import Product from './product.model';
+
+export const doesProductExist = async (req, res, next) => {
+  const { productId } = req.params;
+  try {
+    const product = await Product.findById(productId);
+    if (product) return next();
+    return serverResponse(res, 400, 'Product not found');
+  } catch (error) {
+    return serverResponse(res, 500, error.message);
+  }
+};
+export const isProductValid = async (req, res, next) => {
+  const errors = schemaErrors(productSchema, req.body);
+  if (errors) {
+    return serverResponse(res, 400, errors[0]);
+  }
+  return next();
+};
---
+++ src/utils/schema/product.schema.js
@@ -0,0 +1,39 @@
+import Joi from 'joi';
+
+export const productSchema = Joi.object({
+  name: Joi.string().required().min(10).messages({
+    'any.required': 'Name is required',
+    'string.empty': 'Name is not allowed to be empty',
+    'string.min': 'Name length must be at least 10 characters long',
+  }),
+  price: Joi.number().required().messages({
+    'any.required': 'Price is required',
+    'number.empty': 'Price is not allowed to be empty',
+  }),
+  sku: Joi.number().required().messages({
+    'any.required': 'SKU is required',
+    'number.empty': 'SKU is not allowed to be empty',
+  }),
+  status: Joi.string().valid('QA', 'COMPLETED').messages({
+    'any.required': 'Status is required',
+    'string.empty': 'Status is not allowed to be empty',
+    'any.only': 'Status must be [QA, COMPLETED]',
+  }),
+  bgColor: Joi.string().required().messages({
+    'any.required': 'Background color is required',
+    'string.empty': 'Background color is not allowed to be empty',
+  }),
+  customer: Joi.string().required().messages({
+    'any.required': 'Customer is required',
+    'string.empty': 'Customer is not allowed to be empty',
+  }),
+  description: Joi.string().required().messages({
+    'any.required': 'Description is required',
+    'string.empty': 'Description is not allowed to be empty',
+  }),
+  project: Joi.string().optional().messages({
+    'any.required': 'Project is required',
+    'string.empty': 'Project is not allowed to be empty',
+  }),
+  image: Joi.object({}).optional(),
+}).options({ abortEarly: false });
