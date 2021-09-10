4b06f13f368cdf0f4bca3071a6e482a5f9403bb3
Akimana A Jean d Amour <akimanaja17@gmail.com>
2021-09-10 14:27:35 +0200

    Create a model to keep projects products

---
+++ src/modules/product/projectProduct.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+const projectProductSchema = new Schema(
+  {
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: 'Project',
+      required: true,
+    },
+    website: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true,
+    writeConcern: { w: 'majority', j: true, wtimeout: 1000 },
+  },
+);
+const ProjectProduct = model('ProjectProduct', projectProductSchema);
+
+export default ProjectProduct;
