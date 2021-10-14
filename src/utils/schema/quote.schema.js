ed28509e99d81a0a6ae267c977ba8b5920de2cbf
Akimana A Jean d Amour <akimanaja17@gmail.com>
2021-10-14 21:00:33 +0200

    Modify quote schema for taxes and discount

--- src/utils/schema/quote.schema.js
+++ src/utils/schema/quote.schema.js
@@ -31,6 +31,10 @@ export const quoteSchema = Joi.object({
 // ================ Quote update schema =========================
 
 export const quoteUpdateSchema = Joi.object({
+  projectId: Joi.string().required().messages({
+    'any.required': 'projectId is required',
+    'string.empty': 'projectId is not allowed to be empty',
+  }),
   billingCycle: Joi.string()
     .valid('Monthly', 'Yearly', 'OneTime')
     .required()
@@ -40,15 +44,24 @@ export const quoteUpdateSchema = Joi.object({
       'any.only':
         'Billing Cycle must be one of [Monthly, Yearly, OneTime]',
     }),
-  amount: Joi.number().required().messages({
-    'any.required': 'amount is required',
-    'string.empty': 'amount is not allowed to be empty',
+  tax: Joi.number().required(),
+  discount: Joi.number().required(),
+  isFixed: Joi.boolean().required(),
+  comment: Joi.string(),
+  propasalText: Joi.string().required().messages({
+    'any.required': 'Propasal text is required',
+    'string.empty': 'Propasal text is not allowed to be empty',
   }),
-  status: Joi.string().messages({
-    'any.only': 'Status must be one of [approved, declined]',
-    'string.empty': 'status is not allowed to be empty',
+  customerNote: Joi.string().required().messages({
+    'any.required': 'Customer note is required',
+    'string.empty': 'Customer note is not allowed to be empty',
   }),
-  comment: Joi.string().messages({
-    'string.empty': 'Comment is not allowed to be empty',
+  status: Joi.string().required(),
+  amounts: Joi.object({
+    subtotal: Joi.number(),
+    tax: Joi.number(),
+    discount: Joi.number(),
+    total: Joi.number(),
   }),
+  items: Joi.array(),
 }).options({ abortEarly: false });
