501541923b13c4e7630fb4c84a9e5e6cc065949b
Akimana A Jean d Amour <akimanaja17@gmail.com>
2021-10-14 21:03:32 +0200

    Differentiate quotes from invoices creations

--- src/modules/invoice/invoice.helper.js
+++ src/modules/invoice/invoice.helper.js
@@ -4,6 +4,7 @@ import pdf from 'pdf-creator-node';
 import { sendInvoice } from '../mail/mail.controller';
 import Invoice from '../../database/model/invoice.model';
 import InstanceMaintain from '../../database/maintains/instance.maintain';
+import User from '../../database/model/user.model';
 
 /**
  * This class will contains all helpers function to handle invoice generation
@@ -21,54 +22,77 @@ class InvoiceHelpers {
   }
 
   static async generatePDF(body, isDownload = false) {
-    const html = fs.readFileSync('template.html', 'utf8');
-    let contents =
-      '<div style="font-weight:900;margin-top:00px;text-align:center;">';
-    contents +=
-      body.message || 'Pay the invoice within specified time';
-    contents += '</div>';
+    const { message, type = 'invoice', items = [] } = body;
+    // console.log(items);
+    try {
+      let fileName = `./${type}.pdf`;
+      let html = fs.readFileSync(`${type}.html`, 'utf8');
+      const user = await User.findById(body.userId);
+      let contents =
+        '<div style="font-weight:900;margin-top:00px;text-align:center;">';
+      contents += message;
+      contents += '</div>';
 
-    const options = {
-      rmat: 'A6',
-      orientation: 'landscape',
-      footer: {
-        height: '20mm',
-        contents,
-      },
-    };
+      const options = {
+        rmat: 'A6',
+        orientation: 'landscape',
+        footer: {
+          height: '20mm',
+          contents,
+        },
+      };
 
-    const users = [
-      {
-        orderId: body.orderId,
+      const invoice = {
+        orderId: body.order._id,
         due_date: body.due_date,
-        amount: `$${body.amount?.toLocaleString('en-US')}`,
+        createdAt: body.createdAt,
+        total: `$${body.amounts?.total?.toLocaleString('en-US')}`,
+        subTotal: `$${body.amounts?.subtotal?.toLocaleString(
+          'en-US',
+        )}`,
         projectName: body.project?.name,
         projectType: body.project?.type,
-      },
-    ];
-    const invoiceFileName = './invoice.pdf';
-    const document = { html, data: { users }, path: invoiceFileName };
+        invoiceNumber: body.order.invoiceNumber || 4,
+        status: body.order.status,
+        amountDue: 0,
+      };
 
-    const invoiceDoc = await pdf.create(document, options);
-    if (isDownload) {
-      return invoiceDoc;
-    }
-    fs.readFile(invoiceFileName, async (err, data) => {
-      const attachments = [
-        {
-          filename: 'invoice.pdf',
-          content: data.toString('base64'),
-          type: 'application/pdf',
-          disposition: 'attachment',
-          contentId: body.orderId,
+      const document = {
+        html,
+        data: {
+          invoice,
+          user: user.toObject(),
+          orderNumber: invoice.invoiceNumber,
+          propasalText: body.order.propasalText || '',
+          customerNote: body.order.customerNote || '',
+          items,
         },
-      ];
-      await sendInvoice(
-        body.customerEmail,
-        'This is your invoice reach from Time Capsule 3D',
-        attachments,
-      );
-    });
+        path: fileName,
+      };
+
+      const invoiceDoc = await pdf.create(document, options);
+      if (isDownload) {
+        return invoiceDoc;
+      }
+      fs.readFile(invoiceFileName, async (err, data) => {
+        const attachments = [
+          {
+            filename: 'invoice.pdf',
+            content: data.toString('base64'),
+            type: 'application/pdf',
+            disposition: 'attachment',
+            contentId: body.orderId,
+          },
+        ];
+        await sendInvoice(
+          user.email,
+          'This is your invoice reach from Time Capsule 3D',
+          attachments,
+        );
+      });
+    } catch (error) {
+      throw Error(error);
+    }
   }
 }
 
