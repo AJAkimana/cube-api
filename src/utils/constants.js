a9a67c5a5dddc54d404a867a575696530ec6a3c1
Niyonsaba fridolin <fridolinho@gmail.com>
2022-03-01 13:56:35 +0200

    fix bugs and add default images

---
+++ src/utils/constants.js
@@ -0,0 +1,32 @@
+const skyboxImages = [
+  'lightgrey.png',
+  'spacegrey.png',
+  'wallblack.png',
+  'walllightgrey.png',
+  'wallspacegrey.png',
+  'wallwhite.png',
+  'white.png',
+];
+
+const customArBtnImages = [
+  'blackwhite.png',
+  'evening_bridge.jpg',
+  'evening_in_the_6.jpg',
+  'forest_tree.jpg',
+  'lakeview_sunset.jpg',
+  'milkyway.jpg',
+  'mountain_sunrise.jpg',
+  'studio.jpg',
+];
+
+// eslint-disable-next-line import/prefer-default-export
+export const defaultImages = [
+  ...skyboxImages.map((img) => ({
+    imageType: 'skybox',
+    imageFileName: img,
+  })),
+  ...customArBtnImages.map((img) => ({
+    imageType: 'custom',
+    imageFileName: img,
+  })),
+];
